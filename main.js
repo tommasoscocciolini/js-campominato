b6c8c73a5270d43c21f10e3d6a5469f9f0f16084
tommaso <scocciolinitommaso@gmail.com>
2021-03-09 16:32:22 +0100

    1a versione campo minato con bonus difficoltà

---
+++ main.js
@@ -0,0 +1,62 @@
+var posBombe = [];
+var posFree = [];
+var maxNumber = 0;
+
+difficolta();
+
+//console.log(maxNumber);
+//console.log(bombsGenerator(posBombe, 100));
+
+posBombe = bombsGenerator(posBombe, maxNumber);
+
+var score = gioco(posBombe, posFree, maxNumber);
+
+console.log("punteggio: "+score);
+document.getElementById('score').innerHTML = "Il tuo punteggio è: "+score;
+document.getElementById('bombs').innerHTML = "Le bombe erano: "+posBombe;
+
+
+function randomNumber(min, max) {
+  if (isNaN(min) || isNaN(max)) {
+    console.log("non sono numeri");
+  } else {
+  return Math.floor(Math.random() * (max - min) ) + min;
+  }
+}
+
+//genero num pc non duplicati
+function bombsGenerator(array, max){
+  while (array.length < 16) {
+    var num = randomNumber(1, max);
+
+    if (!array.includes(num)) {
+      array.push(num);
+    }
+  }
+  return array;
+}
+
+function gioco(posBombe, posFree, maxNumber){
+  while (posFree.length < maxNumber - 16) {
+    var numUser = parseInt(prompt("Inserisci numero, evita le BOMBE!"));
+
+    if (!isNaN(numUser) && numUser >= 1 && numUser <= maxNumber && !posFree.includes(numUser)) {
+      if (!posBombe.includes(numUser)) {
+        posFree.push(numUser);
+      } else {
+        return posFree.length;
+      }
+    } else alert("Numero fuori range o già inserito!")
+  } return posFree.length;
+}
+
+function difficolta(){
+  var diffUser = parseInt(prompt("Inserisci livello di difficoltà tra 0 e 2"));
+  if (diffUser==0) {
+    maxNumber=100;
+  } else if (diffUser==1) {
+    maxNumber=80;
+  } else if (diffUser==2) {
+    maxNumber=50;
+  } else alert("valore di difficolta non valido: Inserisci 0, 1 oppure 2")
+}
